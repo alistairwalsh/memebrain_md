4916d2b76cb7c81a39cd416edb64322de28a0bde
Alistair Walsh <alistair.walsh@me.com>
2022-11-11 09:41:14 +1100

    split to lines

--- Home.py
+++ Home.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-with open('medicine_tablets.hpp') as tablets:
-    st.write(tablets.read())
+with open('medicine_tablets.txt', 'r') as tablets:
+    st.write(tablets.read().str.splitlines(keepends=False))
