7ff9140d35c4eab714304c355569aa184e02a395
Alistair Walsh <alistair.walsh@me.com>
2022-11-11 09:53:48 +1100

    just the first bit

--- Home.py
+++ Home.py
@@ -1,17 +1,18 @@
 import streamlit as st
 
 with open('medicine_ampouls.txt', 'r') as infile:
-    ampouls = infile.read().splitlines(keepends=False)
-    st.write(ampouls)
+    ampouls = infile.read()
+    for line in ampouls:
+        st.write(line)
 
-with open('medicine_injectors.txt', 'r') as infile:
-    injectors = infile.read().splitlines(keepends=False)
-    st.write(injectors)
+# with open('medicine_injectors.txt', 'r') as infile:
+#     injectors = infile.read()
+#     st.write(injectors)
 
-with open('medicine_salves.txt', 'r') as infile:
-    salves = infile.read().splitlines(keepends=False)
-    st.write(salves)
+# with open('medicine_salves.txt', 'r') as infile:
+#     salves = infile.read()
+#     st.write(salves)
 
-with open('medicine_tablets.txt', 'r') as infile:
-    tablets = infile.read().splitlines(keepends=False)
-    st.write(tablets)
+# with open('medicine_tablets.txt', 'r') as infile:
+#     tablets = infile.read()
+#     st.write(tablets)
