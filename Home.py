b381cb54d78d12cb3885a89374a54b944bc5d028
Alistair Walsh <alistair.walsh@me.com>
2022-11-11 09:45:55 +1100

    read all files

--- Home.py
+++ Home.py
@@ -1,4 +1,17 @@
 import streamlit as st
 
-with open('medicine_tablets.txt', 'r') as tablets:
-    st.write(tablets.read().str.splitlines(keepends=False))
+with open('medicine_ampouls.txt', 'r') as infile:
+    ampouls = infile.read().splitlines(keepends=False)
+    st.write(ampouls)
+
+with open('medicine_injectors.txt', 'r') as infile:
+    injectors = infile.read().splitlines(keepends=False)
+    st.write(injectors)
+
+with open('medicine_salves.txt', 'r') as infile:
+    salves = infile.read().splitlines(keepends=False)
+    st.write(salves)
+
+with open('medicine_tablets.txt', 'r') as infile:
+    tablets = infile.read().splitlines(keepends=False)
+    st.write(tablets)
