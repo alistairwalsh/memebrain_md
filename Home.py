f70c88bc46a23e98dc8d8a7c79292c7ffe1b1175
Alistair Walsh <alistair.walsh@me.com>
2022-11-11 11:12:27 +1100

    stripping

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ with open('medicine_ampouls.txt', 'r') as infile:
     ampule_text = {}
     for line in infile.readlines():
         if line.strip().startswith(('class')):
-            new_k = line.split()[1].replace('Ampoule','')
+            new_k = line.split()[1].replace('Ampoule','').strip()
             ampule_text[new_k] = []
         elif len(line) < 1 or line.startswith(('{','}')):
             pass
