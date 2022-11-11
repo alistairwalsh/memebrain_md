dbf4de0de382b86e079be0958343a8e340a0d7fb
Alistair Walsh <alistair.walsh@me.com>
2022-11-11 13:42:47 +1100

    try image select

--- Home.py
+++ Home.py
@@ -1,6 +1,13 @@
 import streamlit as st
+from streamlit_image_select import image_select
 import cv2
 from glob import glob
+
+# Hematopoiesis (pronounced “heh-ma-tuh-poy-EE-sus”) is blood cell production
+# Hemostasis refers to normal blood clotting in response to an injury
+# HemologicShock (Hypovolemic shock) is an emergency condition in which severe blood or other fluid loss makes the heart unable to pump enough blood to the body.
+# Hematoma is a bad bruise. It happens when an injury causes blood to collect and pool under the skin.
+# Visceral injuries are injuries to all internal organs
 
 cures = [
     'UseSalve', 
@@ -32,11 +39,24 @@ illness = [
 ]
 
 st.title('Illness')
+illness_images = []
+illness_names = []
 for filename in glob('images/*.png'):
     name = filename.split('/')[-1].split('.')[0]
     if name not in cures:
-        st.write(name)
-        st.image(cv2.imread(filename),caption = name, width=100)
+        illness_images.append(filename)
+        illness_names.append(name)
+        
+
+
+img = image_select(
+    label="Select what ails you",
+    images=[cv2.imread(filename) for filename in illness_images],
+    captions=illness_names
+)
+
+st.write(img)
+st.image(img)
 
 # st.title('Cured')
 # for filename in glob('images/*.png'):
