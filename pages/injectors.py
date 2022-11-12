623da0753c409e7838b1be89e95f2cdc674a358c
Alistair Walsh <alistair.walsh@me.com>
2022-11-12 12:28:31 +1100

    clean key

--- pages/injectors.py
+++ pages/injectors.py
@@ -12,7 +12,7 @@ injector_text = {k:v.split(';')[1:-4] for k, v in [l.split('{', 1) for l in text
 clean_injector_text = {}
 
 for k, v in injector_text.items():
-    clean_injector_text[k] = {kkk.strip():vvv.strip() for kkk,vvv in [vv.split('=') for vv in v] if kkk.strip() not in ("model","hiddenSelections[]",)}
+    clean_injector_text[k.split(' ')[1]] = {kkk.strip():vvv.strip() for kkk,vvv in [vv.split('=') for vv in v] if kkk.strip() not in ("model","hiddenSelections[]",)}
     st.json(clean_injector_text)
 
 st.write(injector_text)
