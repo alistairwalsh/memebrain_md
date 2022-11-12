7abe9d7938bc378759dcb51112f67d0ac2831bc3
Alistair Walsh <alistair.walsh@me.com>
2022-11-12 16:53:09 +1100

    LAYOUT

--- Home.py
+++ Home.py
@@ -103,7 +103,8 @@ for med_type in [clean_injectors_text,clean_ampules_text,clean_salve_text,clean_
     for med_type_k, med_type_v in med_type.items():
         treatments = [k for k in med_type[med_type_k].keys() if k == dict_ill_2_cure[option]]
         if treatments:
-            st.write(med_type[med_type_k]['displayName'].split('_')[1], med_type[med_type_k]['colour'].split('_')[-2])
+            name = med_type[med_type_k]['displayName'].split('_')
+            st.write(f'Type:{name[0]}, Name:{name[1]}','Colour:', med_type[med_type_k]['colour'].split('_')[-2])
 
 
 # st.write(illness_names)
