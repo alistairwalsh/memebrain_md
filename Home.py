63de7a4945129872dfd67d16844fe0f79d475171
Alistair Walsh <alistair.walsh@me.com>
2022-11-12 16:05:49 +1100

    maybe

--- Home.py
+++ Home.py
@@ -4,6 +4,21 @@ import cv2
 from glob import glob
 import itertools
 
+cures = [
+    'UseSalve', 
+    'Stomatchheal', 
+    'Bandage1', 
+    'Painkiller', 
+    'Bandage2', 
+    'Antidepresant', 
+    'Adrenalin', 
+    'Antibiotic', 
+    'Disinfected', 
+    'Radioprotection',
+    "Hemostatic"
+    ]
+
+
 st.title('Illness')
 illness_images = []
 illness_names = []
@@ -57,69 +72,10 @@ def get_text(filename):
 
 clean_injectors_text = get_text('medicine_injectors.txt')
 
-@st.experimental_singleton
-def get_text(filename):
-    with open(filename, 'r') as infile:
-        in_text = {}
-        text = infile.read()
-
-    in_text = {k:v.split(';') for k, v in [l.split('{', 1) for l in text.split('\n\n') if len(l) > 0 ]} # split on newline, split on '{' then split on ';'
-
-    clean_text = {}
-
-    for k, v in in_text.items():
-        clean_text[k.split(' ')[1]] = {kkk.replace('"','').strip():vvv.replace('"','').strip() for kkk,vvv in [vv.split('=') for vv in v if '=' in vv] if kkk.strip() not in ("model","hiddenSelections[]","scope","varQuantityInit","varQuantityMax")}
-
-    for k, v in clean_text.items():
-        if "hiddenSelectionsTextures[]" in clean_text[k].keys():
-            clean_text[k]['colour'] = clean_text[k]["hiddenSelectionsTextures[]"].split('\\')[-1].replace('.paa','').replace('}','').strip()
-            del clean_text[k]["hiddenSelectionsTextures[]"]
-
-    return clean_text
-
 clean_ampules_text = get_text('medicine_ampouls.txt')
-
-@st.experimental_singleton
-def get_text(filename):
-    with open(filename, 'r') as infile:
-        in_text = {}
-        text = infile.read()
-
-    in_text = {k:v.split(';') for k, v in [l.split('{', 1) for l in text.split('\n\n') if len(l) > 0 ]} # split on newline, split on '{' then split on ';'
-
-    clean_text = {}
-
-    for k, v in in_text.items():
-        clean_text[k.split(' ')[1]] = {kkk.replace('"','').strip():vvv.replace('"','').strip() for kkk,vvv in [vv.split('=') for vv in v if '=' in vv] if kkk.strip() not in ("model","hiddenSelections[]","scope","varQuantityInit","varQuantityMax")}
-
-    for k, v in clean_text.items():
-        if "hiddenSelectionsTextures[]" in clean_text[k].keys():
-            clean_text[k]['colour'] = clean_text[k]["hiddenSelectionsTextures[]"].split('\\')[-1].replace('.paa','').replace('}','').strip()
-            del clean_text[k]["hiddenSelectionsTextures[]"]
-
-    return clean_text
 
 clean_salve_text = get_text('medicine_salves.txt')
 
-@st.experimental_singleton
-def get_text(filename):
-    with open(filename, 'r') as infile:
-        in_text = {}
-        text = infile.read()
-
-    in_text = {k:v.split(';') for k, v in [l.split('{', 1) for l in text.split('\n\n') if len(l) > 0 ]} # split on newline, split on '{' then split on ';'
-
-    clean_text = {}
-
-    for k, v in in_text.items():
-        clean_text[k.split(' ')[1]] = {kkk.replace('"','').strip():vvv.replace('"','').strip() for kkk,vvv in [vv.split('=') for vv in v if '=' in vv] if kkk.strip() not in ("model","hiddenSelections[]","scope","varQuantityInit","varQuantityMax")}
-
-    for k, v in clean_text.items():
-        if "hiddenSelectionsTextures[]" in clean_text[k].keys():
-            clean_text[k]['colour'] = clean_text[k]["hiddenSelectionsTextures[]"].split('\\')[-1].replace('.paa','').replace('}','').strip()
-            del clean_text[k]["hiddenSelectionsTextures[]"]
-
-    return clean_text
 clean_tablet_text = get_text('medicine_tablets.txt')
 
 
