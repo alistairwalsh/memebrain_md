dad4d188db77eb5a9ef8dbaf7eff9a59bc6bd90c
Alistair Walsh <alistair.walsh@me.com>
2022-11-12 13:25:06 +1100

    updated all to seperate pages

---
+++ pages/ampules.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from streamlit_image_select import image_select
+import cv2
+from glob import glob
+
+
+@st.experimental_singleton
+def get_text(filename):
+    with open(filename, 'r') as infile:
+        in_text = {}
+        text = infile.read()
+
+    in_text = {k:v.split(';') for k, v in [l.split('{', 1) for l in text.split('\n\n')]} # split on newline, split on '{' then split on ';'
+
+    clean_text = {}
+
+    for k, v in in_text.items():
+        clean_text[k.split(' ')[1]] = {kkk.replace('"','').strip():vvv.replace('"','').strip() for kkk,vvv in [vv.split('=') for vv in v if '=' in vv] if kkk.strip() not in ("model","hiddenSelections[]","scope","varQuantityInit","varQuantityMax")}
+
+    for k, v in clean_text.items():
+        if "hiddenSelectionsTextures[]" in clean_text[k].keys():
+            clean_text[k]['colour'] = clean_text[k]["hiddenSelectionsTextures[]"].split('\\')[-1].replace('.paa','').replace('}','').strip()
+            del clean_text[k]["hiddenSelectionsTextures[]"]
+
+    return clean_text
+
+clean_ampules_text = get_text('medicine_ampules.txt')
+st.json(clean_salve_text)
