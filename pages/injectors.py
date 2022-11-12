e76a4ed0d7b98184138f2084ddd53a48c1e27f73
Alistair Walsh <alistair.walsh@me.com>
2022-11-12 12:08:50 +1100

    list comp dict comp

--- pages/injectors.py
+++ pages/injectors.py
@@ -7,7 +7,7 @@ with open('medicine_injectors.txt', 'r') as infile:
     injector_text = {}
     text = infile.read()
 
-st.write([l.split('{', 1) for l in text.split('\n\n')])
+st.write({k:v.split(';') for k, v in [l.split('{', 1) for l in text.split('\n\n')]})
 
     # for line in infile.readlines():
     #     line = line.strip()
