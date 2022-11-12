c492927837ea400a6dc55a41e93adc111d6c121f
Alistair Walsh <alistair.walsh@me.com>
2022-11-12 13:40:29 +1100

    print raw

--- pages/ampules.py
+++ pages/ampules.py
@@ -4,25 +4,31 @@ import cv2
 from glob import glob
 
 
-@st.experimental_singleton
-def get_text(filename):
-    with open(filename, 'r') as infile:
+with open('medicine_ampouls.txt', 'r') as infile:
         in_text = {}
         text = infile.read()
 
-    in_text = {k:v.split(';') for k, v in [l.split('{', 1) for l in text.split('\n\n')]} # split on newline, split on '{' then split on ';'
+st.write(in_text)
 
-    clean_text = {}
+# @st.experimental_singleton
+# def get_text(filename):
+#     with open(filename, 'r') as infile:
+#         in_text = {}
+#         text = infile.read()
 
-    for k, v in in_text.items():
-        clean_text[k.split(' ')[1]] = {kkk.replace('"','').strip():vvv.replace('"','').strip() for kkk,vvv in [vv.split('=') for vv in v if '=' in vv] if kkk.strip() not in ("model","hiddenSelections[]","scope","varQuantityInit","varQuantityMax")}
+#     in_text = {k:v.split(';') for k, v in [l.split('{', 1) for l in text.split('\n\n')]} # split on newline, split on '{' then split on ';'
 
-    for k, v in clean_text.items():
-        if "hiddenSelectionsTextures[]" in clean_text[k].keys():
-            clean_text[k]['colour'] = clean_text[k]["hiddenSelectionsTextures[]"].split('\\')[-1].replace('.paa','').replace('}','').strip()
-            del clean_text[k]["hiddenSelectionsTextures[]"]
+#     clean_text = {}
 
-    return clean_text
-    
-clean_ampules_text = get_text('medicine_ampouls.txt')
-st.json(clean_ampules_text)
+#     for k, v in in_text.items():
+#         clean_text[k.split(' ')[1]] = {kkk.replace('"','').strip():vvv.replace('"','').strip() for kkk,vvv in [vv.split('=') for vv in v if '=' in vv] if kkk.strip() not in ("model","hiddenSelections[]","scope","varQuantityInit","varQuantityMax")}
+
+#     for k, v in clean_text.items():
+#         if "hiddenSelectionsTextures[]" in clean_text[k].keys():
+#             clean_text[k]['colour'] = clean_text[k]["hiddenSelectionsTextures[]"].split('\\')[-1].replace('.paa','').replace('}','').strip()
+#             del clean_text[k]["hiddenSelectionsTextures[]"]
+
+#     return clean_text
+
+# clean_ampules_text = get_text('medicine_ampouls.txt')
+# st.json(clean_ampules_text)
